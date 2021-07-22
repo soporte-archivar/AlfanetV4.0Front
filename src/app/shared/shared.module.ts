f09d0b5b525517f3195671cb6bdd5abdbbcfad6f
soporte1archivar <soporte1.archivar@gmail.com>
2021-07-22 10:01:06 -0500

    22Julio10Am

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    SidenavComponent
+  ]
+})
+export class SharedModule { }
